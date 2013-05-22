d991357dd48c6ae868c70ec8cbfedc8f376ee936
Alexander Steen <a.steen@fu-berlin.de>
2013-05-22 15:04:36 +0200

    numme ueb5 aufg3 a+b

---
+++ 2013_SoSe/numme/ueb5/InterpolationAD.m
@@ -0,0 +1,32 @@
+function [] = InterpolationAD(f,I,n)
+// Grenzen des Intervalls
+a,b = I(1),I(2);
+// m ist die Anzahl der Gitterpunkte (Grad + 1)
+m = n+1;
+// Genaugkeit des Plots
+genauigkeit = 0.1;
+dist = min(genauigkeit, (b-a)/m);
+
+// Erstelle Gitter:
+// Aequidistanter Gitter a = x0 < x1 < ... < xm = b
+// mit Distanz (b-a)/m
+stuetzSt = [a:(b-a)/m:b];
+// Funktionswerte an den Stuetzstellen
+fstuetzSt  = f(stuetzSt);
+
+// Dazu: erstelle Funktionswerte an allen Stellen
+// der gewaehlten Genauigkeit
+// unter Nutzung von Aitken-Neville
+x  = [a:dist:b] ;
+fx = zeros(1,(b-a)/dist);
+for i = 1:(b-a)/dist
+  fx(i) = AitkenNeville(stuetzSt,fstuetzSt,x(i));
+end;
+
+// Plot
+h = plot(x,fx);
+title('Interpolation durch Polynom n-ten Grades');
+xlabel('x');
+ylabel('p(x)');
+
+saveas(h,'plot.png');
