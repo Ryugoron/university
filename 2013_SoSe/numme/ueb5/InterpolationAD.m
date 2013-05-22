0b73a85653b5d3a08c2fec45ee040b9a6f66a561
Alexander Steen <a.steen@fu-berlin.de>
2013-05-22 15:15:27 +0200

    minor fixes: ceil added

--- 2013_SoSe/numme/ueb5/InterpolationAD.m
+++ 2013_SoSe/numme/ueb5/InterpolationAD.m
@@ -1,29 +1,30 @@
 function [] = InterpolationAD(f,I,n)
-// Grenzen des Intervalls
-a,b = I(1),I(2);
-// m ist die Anzahl der Gitterpunkte (Grad + 1)
+%% Grenzen des Intervalls
+a = I(1);
+b = I(2);
+%% m ist die Anzahl der Gitterpunkte (Grad + 1)
 m = n+1;
-// Genaugkeit des Plots
+%% Genaugkeit des Plots
 genauigkeit = 0.1;
-dist = min(genauigkeit, (b-a)/m);
+dist = min(genauigkeit, ceil((b-a)/m));
 
-// Erstelle Gitter:
-// Aequidistanter Gitter a = x0 < x1 < ... < xm = b
-// mit Distanz (b-a)/m
+%% Erstelle Gitter:
+%% Aequidistanter Gitter a = x0 < x1 < ... < xm = b
+%% mit Distanz (b-a)/m
 stuetzSt = [a:(b-a)/m:b];
-// Funktionswerte an den Stuetzstellen
+%% Funktionswerte an den Stuetzstellen
 fstuetzSt  = f(stuetzSt);
 
-// Dazu: erstelle Funktionswerte an allen Stellen
-// der gewaehlten Genauigkeit
-// unter Nutzung von Aitken-Neville
-x  = [a:dist:b] ;
-fx = zeros(1,(b-a)/dist);
+%% Dazu: erstelle Funktionswerte an allen Stellen
+%% der gewaehlten Genauigkeit
+%% unter Nutzung von Aitken-Neville
+x  = [a:dist:b];
+fx = zeros(1,ceil((b-a)/dist));
 for i = 1:(b-a)/dist
   fx(i) = AitkenNeville(stuetzSt,fstuetzSt,x(i));
 end;
 
-// Plot
+%% Plot
 h = plot(x,fx);
 title('Interpolation durch Polynom n-ten Grades');
 xlabel('x');
