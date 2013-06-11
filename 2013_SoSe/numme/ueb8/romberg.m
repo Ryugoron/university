67a95ce952c47a594bdd785d2cf3e4b9e1b52ebf
Alexander Steen <a.steen@fu-berlin.de>
2013-06-11 20:45:41 +0200

    romberg

--- 2013_SoSe/numme/ueb8/romberg.m
+++ 2013_SoSe/numme/ueb8/romberg.m
@@ -1,18 +1,16 @@
-function [S] = romberg(a,b,f,n)
+function [S] = romberg(a,b,f,h)
 % Gibt den Zahlenwert S der Quadratur zurueck
 %
 % [a,b] Integrationsintervall
 % f zu intigrierende Funktion
-% n anzahl der teilintervalle
-
-%% h(1) = h0
-h = 2.^(-1.*[0:j])*b
+% h Liste der Schrittweiten
+n = length(h);
 
 t = zeros(n,1);
 for i = 1:n
-  
-  t(i) = trapez
+  t(i) = trapQuad(a:h(i):b,f);
   for k = i:-1:1
-    t(k) = t(k+1) + (t(k+1)-t(k))/((h(k)/h(i))^2 - 1)
+    t(k) = t(k+1) + (t(k+1)-t(k))/((h(k)/h(i))^2 - 1);
   end
 end
+S = t(1);
