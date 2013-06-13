2acbe0b257eca947e0ea3990efa86c30835dad15
Alexander Steen <a.steen@fu-berlin.de>
2013-06-13 11:24:39 +0200

    index fix

--- 2013_SoSe/numme/ueb8/romberg.m
+++ 2013_SoSe/numme/ueb8/romberg.m
@@ -8,8 +8,8 @@ n = length(h);
 
 t = zeros(n,1);
 for i = 1:n
-  t(i) = trapQuad(a:h(i):b,f);
-  for k = i:-1:1
+  t(i) = trapQuad([a:h(i):b],f);
+  for k = (i-1):-1:1
     t(k) = t(k+1) + (t(k+1)-t(k))/((h(k)/h(i))^2 - 1);
   end
 end
