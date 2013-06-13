23f47ab7a92c8e14a4baf90fe147aa6633714372
Alexander Steen <a.steen@fu-berlin.de>
2013-06-13 10:52:52 +0200

    aufgabe 1 weiter

--- 2013_SoSe/numme/ueb8/romberg.m
+++ 2013_SoSe/numme/ueb8/romberg.m
@@ -1,14 +1,23 @@
 function [S] = romberg(a,b,f,h)
-% Gibt den Zahlenwert S der Quadratur zurueck
+% Gibt den Zahlenwert S der klassischen
+% Romberg-Quadratur zurueck
 %
 % [a,b] Integrationsintervall
 % f zu intigrierende Funktion
 % h Liste der Schrittweiten
+%  (absteigende Gitterbreite!)
+
+% Anzahl der Gitterbreiten
 n = length(h);
 
 t = zeros(n,1);
+
+%% Berechnung nach Aitken-Neville-Schema
+%% mit Rekursion aus dem Skript
 for i = 1:n
+  %% summierte Trapezregel zur breite h_i
   t(i) = trapQuad(a:h(i):b,f);
+  %% Rekursion
   for k = i:-1:1
     t(k) = t(k+1) + (t(k+1)-t(k))/((h(k)/h(i))^2 - 1);
   end
